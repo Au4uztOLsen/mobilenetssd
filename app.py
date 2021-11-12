2df22a4fadad0518333afabdab4c7c69490ec87f
Au4uztOLsen <89003810+Au4uztOLsen@users.noreply.github.com>
2021-11-12 11:23:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,10 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
             replyObj = TextSendMessage(text="สวัสดีค่า")
+        elif (msg == "เเดกเหล้าป่าว") :
+            replyObj = TextSendMessage(text="รอไรอ่ะ เอาดิ")
+        elif (msg == "KFCหรือMcdonald") :
+            replyObj = TextSendMessage(text="Bonchon")
         else : 
             replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
